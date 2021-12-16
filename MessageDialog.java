f19f66ef91510a1053ef9063ce56970543bdd764
Shuvo Goswami <76766612+shuvogoswami007@users.noreply.github.com>
2021-12-16 11:32:38 +0600

    Add files via upload

---
+++ MessageDialog.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class MessageDialog {
+    public static void main (String[] args){
+    
+        JOptionPane.showMessageDialog(null,"wrong password");
+    }
+    
+    
+}
